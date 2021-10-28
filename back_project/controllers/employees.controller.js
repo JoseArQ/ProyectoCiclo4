f9c4fa9eab1ce78485d27c201018baf283099df7
JoseArQ <arquos.14@gmail.com>
2021-10-28 13:39:04 -0500

    Add routes and controller method for update and remove by id.

--- back_project/controllers/employees.controller.js
+++ back_project/controllers/employees.controller.js
@@ -46,3 +46,48 @@ exports.findOne = function(req, res){
     });
    
 }
+
+exports.update = (req, res) => {
+
+    let employe ={
+        name: req.body.name,
+        lastName_p: req.body.lastName_p,
+        lastName_m: req.body.lastName_m,
+        phone: req.body.phone,
+        email: req.body.email,
+        addres: req.body.addres
+    };
+
+    /* console.log('id: '+req.params.id);
+    console.log('empleado: '+employe); */
+    Employe.findByIdAndUpdate(req.params.id, {$set: employe}, (err) =>{
+
+        if (err){
+            console.error(err);
+            response.succes = false;
+            response.msg = "Error updating employee";
+            res.json(response);
+            return;
+        }
+        response.succes = true;
+        response.msg = "employee update succesfully";
+        res.json(response);
+    });
+}
+
+exports.delete = (req, res) => {
+    Employe.findByIdAndRemove(req.params.id, (err, employe) =>{
+
+        if (err){
+            console.error(err);
+            response.succes = false;
+            response.msg = "Error delete employee";
+            res.json(response);
+            return;
+        }
+        
+        response.succes = true;
+        response.msg = `employee ${employe.name} delete succesfully`;
+        res.json(response);
+    });
+}
--- back_project/routes/employe.router.js
+++ back_project/routes/employe.router.js
@@ -5,5 +5,7 @@ const employeController = require('../controllers/employees.controller');
 router.post('/', employeController.create);
 router.get('/', employeController.find);
 router.get('/:id', employeController.findOne);
+router.put('/:id', employeController.update);
+router.delete('/:id', employeController.delete);
 
 module.exports = router;
