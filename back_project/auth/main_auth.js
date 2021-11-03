eaf854c2cbe1c447c0917cd13e71e1eed710160e
Jose Arroyave <arquos.14@gmail.com>
2021-11-02 22:44:52 -0500

    Delete print token in main_auth.

--- back_project/auth/main_auth.js
+++ back_project/auth/main_auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const auth = (req, res, next) => {
     try {
         const token = req.headers.authorization;
-        console.log(`token is ${token}`);
         const decode = jwt.verify(token, '__recret__');
 
         req.user = decode;
